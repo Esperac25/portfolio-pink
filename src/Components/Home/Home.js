7b01007c98ab231fbb138f8c20192c8ac1943771
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-03-08 13:14:43 -0800

    editing about me section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -16,20 +16,26 @@ const Home = () => {
 				<p>
 				    I am a Fullstack Web Developer
 					based in Seattle, WA and I enjoy making vibrant and creative
-					applications.
+					applications. 
 				</p>
 
 				<p>
 					After working as a Medical Assistant for a few years, I 
 					realized that I enjoyed the technical aspects over the clinical aspects of the job.
-                    This inspired me to make the move to Tech.
+                    This inspired me to make the move to Tech, I enrolled into Thinkful's Engineering Immersion program and learned the fundamentals or Fullstack JavaScript programming.
 				</p>
-
+				<p>
+					In the immersion program, I gained experience building full stack applications that where regulated in a test-driven environment as well as working in teams and collaborating.
+				</p>
 				<p>
 					With my experience in the medical field, I’m passionate about
 					building products that touch the lives of others. I've helped
 					doctors/providers deliver great patient care. Now, I want to build web
 					applications that allow users to be better.
+				</p>
+				<p>
+					I am open to collaboration, employment opportunities, or questions about my journey so far.
+					
 				</p>
 
 				<p>
--- src/Components/Projects/store.js
+++ src/Components/Projects/store.js
@@ -6,7 +6,7 @@ const store = {
         {
             img: [reflect],
             title: 'Reflect',
-            description: 'A Fullstack application designed for users to save private journal entries and view them in their personal blog',
+            description: "Reflect was created as a full stack journal app, where users can add, edit, delete and search through their private journal entries in a blog like interface.",
             stack: 'React, HTML5, CSS3, PostgreSQL, Express, Node',
             repo: 'https://github.com/Esperac25/reflect-client',
             live: 'https://reflect-inhdu56ty-esperac25.vercel.app/',
@@ -14,7 +14,7 @@ const store = {
         {
             img: [weekly],
             title: 'Weekly',
-            description: 'A Fullstack application designed to help users track and organize tasks for their week.',
+            description: 'Weekly was created as a full stack application to help users keep track of to-do items for their week, users can add, delete and view items in their weekly view.',
             stack: 'React, HTML5, CSS3, PostgreSQL, Express, Node',
             repo: 'https://github.com/Esperac25/weekly-client',
             live: 'https://weekly-client-app.vercel.app/',
@@ -22,7 +22,7 @@ const store = {
         {
             img: [quiz],
             title: 'GeoQuiz',
-            description: 'A group made jquery application designed as a fun quiz to help users practice geography',
+            description: 'GeoQuiz was a group made jQuery application, created to help users study USA states and capitols.',
             stack: 'HTML5, CSS3, jQuery',
             repo: 'https://github.com/Esperac25/Geography-Quiz-App',
             live: 'https://jpol95.github.io/quiz-app/',
